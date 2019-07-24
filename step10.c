5e5c23a5c9d74b5b4b43e5a635af538999d44638
alf0811 <alf@sfc.wide.ad.jp>
2019-07-24 17:50:10 +0900

    step10_fix

--- step10.c
+++ step10.c
@@ -7,7 +7,7 @@ int main(void)
     do{
         printf("テストの点数を入力してください：");
         scanf("%d",&point);
-    } while (point > 100);
+    } while (point < 0 || point > 100);
 
     printf("テストの点数は%d点です。\n",point);
 
