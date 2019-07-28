c0a7e3091605597e961c4389ab2a6822d68b6527
alf0811 <alf@sfc.wide.ad.jp>
2019-07-28 14:50:38 +0900

    generation2_fix2

--- lifegame.c
+++ lifegame.c
@@ -34,7 +34,7 @@ int main()
                 cells[i][j] = 1;
             } else if(cells[i][j] == 0 && cells[i-1][j-1] + cells[i][j-1] + cells[i+1][j-1] + cells[i-1][j] + cells[i+1][j] + cells[i-1][j+1] + cells[i][j+1] + cells[i+1][j+1] != 3){
                 cells[i][j] = 0;
-            } else if(cells[i][j] == 1 && 2 <= cells[i-1][j-1] + cells[i][j-1] + cells[i+1][j-1] + cells[i-1][j] + cells[i+1][j] + cells[i-1][j+1] + cells[i][j+1] + cells[i+1][j+1] <= 3){
+            } else if(cells[i][j] == 1 && 2 <= cells[i-1][j-1] + cells[i][j-1] + cells[i+1][j-1] + cells[i-1][j] + cells[i+1][j] + cells[i-1][j+1] + cells[i][j+1] + cells[i+1][j+1] && cells[i-1][j-1] + cells[i][j-1] + cells[i+1][j-1] + cells[i-1][j] + cells[i+1][j] + cells[i-1][j+1] + cells[i][j+1] + cells[i+1][j+1] <= 3){
                 cells[i][j] = 1;
             } else {
                 cells[i][j] = 0;
