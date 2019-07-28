0f95617d43a338f448049734f6a5633e1d1e71c7
alf0811 <alf@sfc.wide.ad.jp>
2019-07-28 13:24:10 +0900

    rand

--- lifegame.c
+++ lifegame.c
@@ -2,14 +2,33 @@
 
 /*
     全体を保持する配列を作る
-    周りのマスの状況を判断する
-    判断結果によって自身の増すの色を変化させる
+
+    ループ[周りのマスの状況を判断する
+        判断結果によって自身の増すの色を変化させる]
     一定時間ごとに調査する
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+    int cells[10][10];
+    int a;
+    srand((unsigned)time(NULL));
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            cells[i][j] = a;
+            a = rand()%2;
+            if(j < 9){
+                printf("%d",cells[i][j]);
+            } else {
+                printf("%d\n",cells[i][j]);
+            }
+        }
+    }
+
     return 0;
 }
+
